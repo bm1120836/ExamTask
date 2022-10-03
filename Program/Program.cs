809435eb1aa21c93c57be3770f5223385e71dac4
BM <bm1120836@yandex.ru>
2022-10-03 20:37:20 +0300

    Добавил файл с решением

---
+++ Program/Program.cs
@@ -0,0 +1,27 @@
+﻿// Решение задачи
+
+string[] array1 = new string[4]{"123", "25", "tokyo", "new"};
+string[] array2 = new string[array1.Length];
+void NewArray (string[] array1, string[] array2)
+    {
+        int count=0;
+        for (int i=0; i < array1.Length; i++)
+        {
+            if(array1[i].Length <= 3)
+            {
+                array2[count] = array1[i];
+                count++;
+            }
+        }
+    }
+void PrintArray(string[] array)
+{
+    for(int i=0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+NewArray(array1, array2);
+PrintArray(array1);
+PrintArray(array2);
